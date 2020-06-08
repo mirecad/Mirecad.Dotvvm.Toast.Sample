72d57ee84010c6ec610b1385fcbe7f9b92ad2158
Miroslav Adamec <miroslav.adamec@invia.cz>
2020-06-08 17:25:43 +0200

    Initial commit

---
+++ Mirecad.Dotvvm.Toast.Sample/Model/Toast.cs
@@ -0,0 +1,9 @@
+﻿namespace Mirecad.Dotvvm.Toast.Sample.Model
+{
+    public class Toast
+    {
+        public bool IsDisplayed { get; set; } = true;
+
+        public string Content { get; set; }
+    }
+}
---
+++ Mirecad.Dotvvm.Toast.Sample/ViewModels/DefaultViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Mirecad.Dotvvm.Toast.Sample.ViewModels
+{
+    public class DefaultViewModel : MasterPageViewModel
+    {
+		public string Title { get; set;}
+
+        public List<Model.Toast> Toasts { get; set; } = new List<Model.Toast>();
+
+		public DefaultViewModel()
+		{
+			Title = "Hello from DotVVM!";
+			Toasts.Add(new Model.Toast()
+            {
+                Content = "toast1"
+            });
+            Toasts.Add(new Model.Toast()
+            {
+                Content = "toast2"
+            });
+        }
+    }
+}
